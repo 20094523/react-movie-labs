47347873c605eeb53fd6acb293b5729fc4215404
Dominik Martynski <74902424+20094523@users.noreply.github.com>
2024-07-19 16:11:26 +0100

    Movie Details coming from API.

--- movies/src/components/movieDetails/index.js
+++ movies/src/components/movieDetails/index.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, {useState, useEffect}  from "react";
+import { useParams } from 'react-router-dom';
 import Chip from "@mui/material/Chip";
 import Paper from "@mui/material/Paper";
 import AccessTimeIcon from "@mui/icons-material/AccessTime";
